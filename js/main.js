aa2f1f446b86a43f095ade88825d761ee9dee4b2
MichelleDuque <michelled_1994@hotmail.com>
2023-03-31 03:09:24 -0400

    changes were added to html css and js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ console.log("JS is working")
 
 let bioLinks = document.querySelectorAll(".characters div a"),
     theLightBox = document.querySelector("#lightbox"),
+    theLightBox2 = document.querySelector("#lightbox"),
     imageProcess = document.querySelector("#imageprocess"),
-    closeLightbox = document.querySelector(".lightbox_close")
+    closeLightbox = document.querySelector(".lightbox_close"),
+    savepos;
 
 function getHeroData(){
     //change the lightbox content to match the hero data
@@ -11,15 +13,19 @@ function getHeroData(){
     theLightBox.querySelector(".lb_heading").textContent = process[this.dataset.member].name;
     theLightBox.querySelector(".lb_text").textContent = process[this.dataset.member].bio;
     imageProcess.src = process[this.dataset.member].imageprocess;
+    theLightBox2.style.display = "block";
+
     
     //change any other elemen's content using the same method - select it, add the object's data to it
     // as textContent, src attributes... whatever you need to change
 }
 
-bioLinks.forEach(link => link.addEventListener("click", getHeroData));
-
-
-
-function openLightBox(event){
-    event.preventDefault();
+function backtoImage(){
+    theLightBox2.style.display = "none";
 }
+
+bioLinks.forEach(link => link.addEventListener("click", getHeroData));
+closeLightbox.addEventListener("click", backtoImage);
+
+
+
