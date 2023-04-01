89202cbc913636ef97def8af53cf73fab0ee0dad
romeroc1996m <camilaromero.9603@gmail.com>
2023-03-31 20:41:22 -0400

    adding changes
    adding changes

--- js/process.js
+++ js/process.js
@@ -1,7 +1,8 @@
 let process = {
     beer:{
         name: "Dr Nut",
-        
+        bio: "Introducing Dr. Nut Almond, even our company started in New Orleans, USA, a few decades ago. This time we switched heads to Canada, working alongside local producers to discover our process and the way we achieve the best quality.",
+        imageprocess:"images/brewing_process_mobile.svg"
     },
     mashing:{
         name: "Mashing",
